146f37a165e8090b9fbf26d12c690def3966d5ca
realeve <realeve@qq.com>
2017-02-25 03:23:21 +0800

    add demo

---
+++ dist/js/chartOption.js
@@ -0,0 +1,450 @@
+function getGoodsDistribOption(data) {
+  var axis = {
+    x: [],
+    y: [],
+    y2: []
+  };
+  axis.y2.push(data[0].value);
+  data.map(function(item, i) {
+    axis.x.push(item.name + 500);
+    axis.y.push(item.value);
+    if (i > 0) {
+      var sum = axis.y2[i - 1] + data[i].value;
+      axis.y2[i] = sum;
+    }
+  });
+  var sum = axis.y2[data.length - 1];
+  axis.y2 = axis.y2.map(function(item) {
+    return (item / sum * 100).toFixed(2);
+  });
+
+  return {
+    title: {
+      text: '商品价格分布'
+    },
+    tooltip: {
+      trigger: 'axis'
+    },
+    legend: {
+      x: 'center',
+      data: ['商品数量', '百分位']
+    },
+    grid: {
+      left: '3%',
+      right: '4%',
+      bottom: '4%',
+      containLabel: true
+    },
+    toolbox: {
+      feature: {
+        saveAsImage: {}
+      }
+    },
+    xAxis: {
+      name: '商品价格',
+      type: 'category',
+      //boundaryGap: true,
+      data: axis.x
+    },
+    yAxis: [{
+      name: '单价',
+      type: 'value'
+    }, {
+      name: '百分比',
+      nameLocation: 'end',
+      max: 100,
+      type: 'value'
+    }],
+    series: [{
+      name: '商品数量',
+      type: 'line',
+      smooth: true,
+      data: axis.y
+    }, {
+      name: '百分位',
+      type: 'line',
+      yAxisIndex: 1,
+      smooth: true,
+      data: axis.y2
+    }]
+  };
+}
+
+function getRecordMapOptionByProvince(data) {
+  return {
+    backgroundColor: '#404a59',
+    title: {
+      text: '2015-2017全国各省市订单数',
+      left: 'center',
+      textStyle: {
+        color: '#fff'
+      }
+    },
+    tooltip: {
+      trigger: 'item'
+    },
+    visualMap: {
+      min: 0,
+      max: 50000,
+      left: 'left',
+      top: 'bottom',
+      text: ['高', '低'],
+      textStyle: {
+        color: '#fff'
+      },
+      calculable: true,
+      color: ['#aE7CeC', '#6a71ba', "#2988b8"]
+    },
+    toolbox: {
+      show: true,
+      orient: 'vertical',
+      left: 'right',
+      top: 'center',
+      feature: {
+        dataView: {
+          readOnly: false
+        },
+        restore: {},
+        saveAsImage: {}
+      }
+    },
+    series: [{
+      type: 'map',
+      mapType: 'china',
+      roam: false,
+      label: {
+        normal: {
+          show: true
+        },
+        emphasis: {
+          show: true
+        }
+      },
+      data: data
+    }]
+  };
+}
+
+function getRegisterMapOptionByProvince(data) {
+  return {
+    backgroundColor: '#404a59',
+    title: {
+      text: '2015-2017全国各省市注册用户数',
+      left: 'center',
+      textStyle: {
+        color: '#fff'
+      }
+    },
+    tooltip: {
+      trigger: 'item'
+    },
+    visualMap: {
+      min: 0,
+      max: 3000,
+      left: 'left',
+      top: 'bottom',
+      text: ['高', '低'],
+      textStyle: {
+        color: '#fff'
+      },
+      calculable: true,
+      color: ['#aE7CeC', '#6a71ba', "#2988b8"]
+    },
+    toolbox: {
+      show: true,
+      orient: 'vertical',
+      left: 'right',
+      top: 'center',
+      feature: {
+        dataView: {
+          readOnly: false
+        },
+        restore: {},
+        saveAsImage: {}
+      }
+    },
+    series: [{
+      type: 'map',
+      mapType: 'china',
+      roam: false,
+      label: {
+        normal: {
+          show: true
+        },
+        emphasis: {
+          show: true
+        }
+      },
+      data: data
+    }]
+  };
+}
+
+function getRecordOptionByProvince(data, dataRegister) {
+  var axis = {
+    x: [],
+    y: [],
+    y2: []
+  };
+  var find;
+  data.forEach((item) => {
+    axis.x.push(item.name);
+    axis.y.push(item.value);
+    find = false;
+    for (var i = 0; !find && i < dataRegister.length; i++) {
+      if (item.name == dataRegister[i].name) {
+        axis.y2.push(dataRegister[i].value);
+        find = true;
+      }
+    }
+  });
+
+  return {
+    title: {
+      text: '2015-2017全国各省市订单数及注册用户数'
+    },
+    tooltip: {
+      trigger: 'axis'
+    },
+    legend: {
+      x: 'center',
+      data: ['订单数', '注册用户数']
+    },
+    grid: {
+      left: '3%',
+      right: '8%',
+      bottom: '4%',
+      containLabel: true
+    },
+    toolbox: {
+      feature: {
+        saveAsImage: {}
+      }
+    },
+    yAxis: {
+      type: 'category',
+      boundaryGap: true,
+      data: axis.x
+    },
+    xAxis: [{
+      name: '订单数',
+      type: 'value'
+    }, {
+      name: '用户数',
+      type: 'value'
+    }],
+    series: [{
+      name: '订单数',
+      type: 'bar',
+      data: axis.y
+    }, {
+      name: '注册用户数',
+      type: 'bar',
+      data: axis.y2,
+      xAxisIndex: 1
+    }]
+  };
+}
+
+function getRecordOption(data, title = '每日', chartType = 'line') {
+  var axis = {
+    x: [],
+    y: [],
+    y2: []
+  };
+  data.forEach((item) => {
+    axis.x.push(item._id);
+    axis.y.push(item.saleNum);
+    axis.y2.push((item.saleValue / 10000).toFixed(2));
+  });
+
+  return {
+    title: {
+      text: title + '销售量及销售额对比'
+    },
+    tooltip: {
+      trigger: 'axis'
+    },
+    legend: {
+      x: 'center',
+      data: ['销售量', '销售额']
+    },
+    grid: {
+      left: '3%',
+      right: '4%',
+      bottom: '8%',
+      containLabel: true
+    },
+    toolbox: {
+      feature: {
+        saveAsImage: {}
+      }
+    },
+    xAxis: {
+      type: 'category',
+      boundaryGap: chartType == 'bar',
+      data: axis.x
+    },
+    yAxis: [{
+      type: 'value',
+      splitLine: {
+        show: false
+      }
+    }, {
+      name: '销售额(万元)',
+      nameLocation: 'end',
+      type: 'value',
+      splitLine: {
+        show: false
+      }
+    }],
+    dataZoom: [{
+      type: 'inside',
+      start: 0,
+      end: 100
+    }, {
+      start: 0,
+      end: 100,
+      handleIcon: 'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
+      handleSize: '80%',
+      handleStyle: {
+        color: '#fff',
+        shadowBlur: 3,
+        shadowColor: 'rgba(0, 0, 0, 0.6)',
+        shadowOffsetX: 2,
+        shadowOffsetY: 2
+      }
+    }],
+    series: [{
+      name: '销售量',
+      type: chartType,
+      data: axis.y,
+      smooth: true,
+      maxWidth: 40
+    }, {
+      name: '销售额',
+      type: chartType,
+      yAxisIndex: 1,
+      data: axis.y2,
+      smooth: true,
+      maxWidth: 40
+    }]
+  };
+}
+
+function getGoodsOption(data, title) {
+  var axis = {
+    x: [],
+    y: []
+  };
+  data.forEach((item) => {
+    axis.x.push(item._id);
+    axis.y.push((item.saleValue / 10000).toFixed(2));
+  });
+
+  return {
+    title: {
+      text: title + '销售详情'
+    },
+    tooltip: {
+      trigger: 'axis'
+    },
+    grid: {
+      left: '3%',
+      right: '4%',
+      bottom: '8%',
+      containLabel: true
+    },
+    toolbox: {
+      feature: {
+        saveAsImage: {}
+      }
+    },
+    xAxis: {
+      type: 'category',
+      boundaryGap: false,
+      data: axis.x
+    },
+    yAxis: [{
+      name: '销售额(万元)',
+      type: 'value',
+      splitLine: {
+        show: false
+      }
+    }],
+    dataZoom: [{
+      type: 'inside',
+      start: 0,
+      end: 100
+    }, {
+      start: 0,
+      end: 100,
+      handleIcon: 'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
+      handleSize: '80%',
+      handleStyle: {
+        color: '#fff',
+        shadowBlur: 3,
+        shadowColor: 'rgba(0, 0, 0, 0.6)',
+        shadowOffsetX: 2,
+        shadowOffsetY: 2
+      }
+    }],
+    series: [{
+      name: '销售额',
+      type: 'line',
+      data: axis.y,
+      smooth: true,
+      maxWidth: 40
+    }]
+  };
+}
+
+function getDistribOption(data) {
+  return {
+    title: {
+      text: '产品构成(按主题及材质分类)',
+      x: 'center'
+    },
+    tooltip: {
+      trigger: 'item',
+      formatter: "{a} <br/>{b} : {c} ({d}%)"
+    },
+    toolbox: {
+      show: true,
+      feature: {
+        mark: {
+          show: true
+        },
+        dataView: {
+          show: true,
+          readOnly: false
+        },
+        magicType: {
+          show: true,
+          type: ['pie', 'funnel']
+        },
+        restore: {
+          show: true
+        },
+        saveAsImage: {
+          show: true
+        }
+      }
+    },
+    calculable: true,
+    series: [{
+      name: '按主题',
+      type: 'pie',
+      radius: [30, '50%'],
+      center: ['25%', '50%'],
+      data: data.theme
+    }, {
+      name: '按材质',
+      type: 'pie',
+      radius: [30, '50%'],
+      center: ['75%', '50%'],
+      data: data.material
+    }]
+  };
+
+}
---
+++ dist/js/coin.js
@@ -0,0 +1,163 @@
+const CHART_NUM = 10;
+const store = new Vuex.Store({
+	state: {}
+});
+
+var app = new Vue({
+	el: '#app',
+	data: {
+		selected: '',
+		chart: [],
+		saleInfo: {
+			num: 0,
+			sum: 0,
+			avgPrice: 0,
+			users: 37447,
+			orderNum: 218404,
+			salesReturn: 3239,
+			goodsNum: 140
+		},
+		popular: {
+			byValue: [],
+			byNum: []
+		}
+	},
+	watch: {
+		selected: function(val) {
+			var title = this.popular.byValue[val].name;
+			var goodsId = this.popular.byValue[val].goodsId;
+			axios.get('./data/goods/goods' + goodsId + '.min.json')
+				.then((res) => {
+					var data = res.data;
+					this.chart[8].setOption(getGoodsOption(data, title));
+				});
+		}
+	},
+	created: function() {
+
+		for (var i = 0; i <= CHART_NUM; i++) {
+			this.chart[i] = echarts.init(document.getElementById('chart' + i));
+		}
+		//销量
+		//axios.get('/coin/static/popular/0')
+		axios.get('./data/popular0.min.json')
+			.then((res) => {
+				var data = res.data;
+				updatePopular(data, app.popular.byNum);
+			});
+
+		//销售额
+		//axios.get('/coin/static/popular/1')
+		axios.get('./data/popular1.min.json')
+			.then((res) => {
+				var data = res.data;
+				updatePopular(data, app.popular.byValue);
+			});
+
+		//axios.get('/coin/static/theme')
+		axios.get('./data/bytheme.min.json')
+			.then((res) => {
+				var data = res.data;
+				var option = getRecordOption(data, '各主题', 'bar');
+				delete option.dataZoom;
+				this.chart[6].setOption(option);
+			});
+
+		axios.get('./data/saleByUser.min.json')
+			.then((res) => {
+				var data = res.data;
+				var option = getRecordOption(data, '消费金额Top50 会员');
+				//delete option.dataZoom;
+				this.chart[9].setOption(option);
+			});
+
+		//产品分布 | 价格、主题、材质
+		axios.get('./data/product-distrib.min.json')
+			.then((response) => {
+				var distrib = response.data;
+				this.chart[0].setOption(getGoodsDistribOption(distrib.price));
+				this.chart[10].setOption(getDistribOption(distrib));
+			})
+			.catch(function(error) {
+				console.log(error);
+			});
+
+		//axios.get('/coin/static/date/10')
+		axios.get('./data/byday.min.json')
+			.then((res) => {
+				var data = res.data;
+				var option = getRecordOption(data, '每日');
+				this.chart[3].setOption(option);
+			});
+
+		//axios.get('/coin/static/date/7')
+		axios.get('./data/bymonth.min.json')
+			.then((res) => {
+				var data = res.data;
+				var option = getRecordOption(data, '每月');
+				delete option.dataZoom;
+				this.chart[2].setOption(option);
+			});
+
+		//axios.get('/coin/static/date/4')
+		axios.get('./data/byyear.min.json')
+			.then((res) => {
+				var data = res.data;
+
+				upateSaleDate(data);
+
+				var option = getRecordOption(data, '每年');
+				delete option.dataZoom;
+				this.chart[1].setOption(option);
+			});
+
+		//axios.get('/coin/static/province')
+		axios.get('./data/order-province-map.min.json')
+			.then((res) => {
+				var data = res.data;
+				axios.get('./data/register-province-map.min.json')
+					.then((res) => {
+						var dataRegister = res.data;
+						this.chart[7].setOption(getRegisterMapOptionByProvince(dataRegister));
+						this.chart[4].setOption(getRecordOptionByProvince(data, dataRegister));
+					});
+			});
+
+		axios.get('./data/order-province-map.min.json')
+			.then((res) => {
+				var data = res.data;
+				this.chart[5].setOption(getRecordMapOptionByProvince(data));
+			});
+	}
+});
+
+function updatePopular(data, watchData) {
+	app.$nextTick(function() {
+		data.map((item) => {
+			watchData.push({
+				name: item._id.name,
+				price: item._id.price,
+				count: item.saleNum,
+				sales: (item.saleValue / 10000).toFixed(2),
+				theme: item._id.theme,
+				img: item._id.img[0],
+				goodsId: item._id.goodsId,
+				url: 'http://item.chinagoldcoin.net/product_detail_' + item._id.goodsId + '.html'
+			});
+		});
+	});
+}
+
+function upateSaleDate(data) {
+	var num = 0,
+		sum = 0;
+	data.map(function(item) {
+		num += item.saleNum;
+		sum += item.saleValue;
+	});
+	app.$nextTick(function() {
+		app.saleInfo.avgPrice = (sum / num).toFixed(2);
+		app.saleInfo.sum = (sum / 10000).toFixed(2);
+		app.saleInfo.num = num;
+	});
+}
